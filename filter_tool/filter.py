55fad8506d5612e65be36d8233488956ce7a0820
Marvin71A178 <aazz282828@gmail.com>
2022-10-26 16:46:14 +0800

    test

--- filter_tool/filter.py
+++ filter_tool/filter.py
@@ -13,12 +13,14 @@ def compare(key_list, content_list):
 
 # all key in output
 
-
+# 課程網址
+# 初賽 複賽 線上賽 線下賽
+# 活動訊息
 def fil_content(content, dic):
     f = open('./filter_tool/filter_key_words.json', "r", encoding="utf-8")
     data = json.load(f)
     f.close()
-    dic["content"] = content
+    dic["content"] = content.replace(" ", "")
     content_in_line = content.split("\n")
     dic["activity_beg_time"] = compare(
         data["activity_beg_time"], content_in_line)
@@ -26,20 +28,20 @@ def fil_content(content, dic):
         data["activity_end_time"], content_in_line)
     dic["apply_beg_time"] = compare(data["apply_beg_time"], content_in_line)
     dic["apply_end_time"] = compare(data["apply_end_time"], content_in_line)
+    dic["apply_int_site"] = compare(data["apply_int_site"], content_in_line)
     dic["contact_info"] = compare(data["contact_info"], content_in_line)
     dic["cost"] = compare(data["cost"], content_in_line)
     dic["host"] = compare(data["host"], content_in_line)
     dic["picture"] = compare(data["picture"], content_in_line)
     dic["position"] = compare(data["position"], content_in_line)
     dic["recruitment_obj"] = compare(data["recruitment_obj"], content_in_line)
-    dic["sources"] = compare(data["sources"], content_in_line)
+    if dic["sources"] == None:
+        dic["sources"] = compare(data["sources"], content_in_line)
     dic["subtitle"] = compare(data["subtitle"], content_in_line)
     dic["tag"] = compare(data["tag"], content_in_line)
     return
 
 # handle index.json
-
-
 def fil_obj(obj):
     out_dic = {}
     out_dic["sources"] = obj["url"]
