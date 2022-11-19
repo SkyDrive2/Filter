9b3264119de8b1710d6290b3096bbea2933d992c
Marvin71A178 <aazz282828@gmail.com>
2022-11-19 20:27:42 +0800

    test

--- re_filter_tool/filter.py
+++ re_filter_tool/filter.py
@@ -35,15 +35,17 @@ def update_only_one():
     b.close()
 
 
+def ex_branch_regular_expression(name, content):
+
+    return
+
+
 def compare(key_list, content_list, type):
     value_list = []
     for content in content_list:
         for key in key_list:
             if key in content:
-                if type != "Tags":
-                    value_list.append(str(content))
-                else:
-                    value_list.append(str(key))
+                value_list.append(str(key))
     if len(value_list) == 0:
         return None
     else:
@@ -71,38 +73,44 @@ def tags_filter(content):
     else:
         return tag_list
 
-def to_date_type(name,list):
+
+def to_date_type(name, list):
     output_list = []
-    if list == None: return list
+    if list == None:
+        return list
     for line in list:
         if (name == "ApplyStart") | (name == "DateStart"):
             if "即日" in line:
                 output_list.append("自即日起")
             else:
-                
+
                 split_list = re.split("[^0-9]", line)
                 remove_split_list = [i for i in split_list if i != ""]
-                
+
                 if len(remove_split_list) == 3:
                     if int(remove_split_list[0]) < 1911:
-                        remove_split_list[0] = str(int(remove_split_list[0])+1911)
-                        
-                    date = str(datetime.date(int(remove_split_list[0]),int(remove_split_list[1]),int(remove_split_list[2])))
+                        remove_split_list[0] = str(
+                            int(remove_split_list[0])+1911)
+
+                    date = str(datetime.date(int(remove_split_list[0]), int(
+                        remove_split_list[1]), int(remove_split_list[2])))
                     output_list.append(date)
                 elif len(remove_split_list) == 2:
                     if remove_split_list[1] == 124:
                         output_list.append(line)
                         return output_list
-                    remove_split_list.insert(0,"1999")
+                    remove_split_list.insert(0, "1999")
                     print(remove_split_list)
                     print("============")
-                    date = str(datetime.date(int(remove_split_list[0]),int(remove_split_list[1]),int(remove_split_list[2])))
+                    date = str(datetime.date(int(remove_split_list[0]), int(
+                        remove_split_list[1]), int(remove_split_list[2])))
                     output_list.append(date)
-                else:    
+                else:
                     output_list.append(line)
         elif (name == "ApplyEnd") | (name == "DateEnd"):
             return list
     return output_list
+
 
 def regular_expression(branch_name, content, only_one):
     data = load_json_folder('./re_filter_tool/projects/branch_key.json')
@@ -110,11 +118,13 @@ def regular_expression(branch_name, content, only_one):
     to_datetype_name = ["DateStart", "DateEnd", "ApplyStart", "ApplyEnd"]
     key_list = data[branch_name].keys()
     branch["BranchName"] = branch_name
-    if only_one: branch_name = "OnlyOne"
+    if only_one:
+        branch_name = "OnlyOne"
     for output_key in key_list:
         branch[output_key] = None
         for key in data[branch_name][output_key]:
             findall_list = re.findall(key, content)
+            # print(findall_list)
             if len(findall_list) == 0:
                 findall_list = None
             if branch[output_key] == None:
@@ -127,36 +137,11 @@ def regular_expression(branch_name, content, only_one):
         #     branch[output_key] = to_date_type(output_key,branch[output_key])
     return branch
 
-
-def fil_except_branch(content, ori_sources):
-    data = load_json_folder('./re_filter_tool/projects/ex_branch_key.json')
-    content_in_line = content.split("\n")
-    except_branch_dic = {}
-    except_branch_dic["Connection"] = compare(
-        data["connection"], content_in_line, "Connection")
-    except_branch_dic["Holder"] = compare(
-        data["holder"], content_in_line, "Holder")
-    except_branch_dic["Objective"] = compare(
-        data["object"], content_in_line, "Object")
-    except_branch_dic["Subtitle"] = compare(
-        data["subtitle"], content_in_line, "Subtitle")
-    update_sources = compare(data["sources"], content_in_line, "Sources")
-    if ori_sources == None:
-        except_branch_dic["Sources"] = update_sources
-    else:
-        if update_sources != None:
-            except_branch_dic["Sources"] = update_sources.append(
-                "ori_sources : " + ori_sources)
-    except_branch_dic["Tags"] = tags_filter(content)
-    return except_branch_dic
-
-
 def fil_branch(content):
     content.replace("\n", "%")
-    data = load_json_folder('./re_filter_tool/projects/ex_branch_key.json')
-
+    branch_name_data = ["初賽", "複賽", "決賽", "線上賽", "線下賽"]
     branch_name_list = []
-    for branch_name in data["branch_name"]:
+    for branch_name in branch_name_data:
         if (branch_name in content):
             branch_name_list.append(branch_name)
 
@@ -173,16 +158,52 @@ def fil_branch(content):
     return branch_dic
 
 
+def fil_ex_branch(content, ori_sources):
+    data = load_json_folder('./re_filter_tool/projects/ex_branch_key.json')
+    ex_branch_dic = {}
+    content = "%"+ content.replace("\n","%↑%") +"%"
+    content_list = content.split("↑")
+    ex_branch_name_data = ["Connection", "Holder","Location","Objective","Sources","Subtitle"]
+    for key in ex_branch_name_data:
+        ex_branch_dic[key] = []
+        for value in data[key]:
+            for content_in_line in content_list:
+                findall_list = re.findall(value,content_in_line)
+                if len(findall_list)!= 0:
+                    ex_branch_dic[key].extend(findall_list)
+        if len(ex_branch_dic[key]) == 0:
+            ex_branch_dic[key] = None
+    if ex_branch_dic["Sources"] != None:
+        ex_branch_dic["Sources"] = ex_branch_dic["Sources"] + ori_sources
+    else:
+        ex_branch_dic["Sources"] = ori_sources
+    if len(ex_branch_dic["Sources"]) == 0:
+        ex_branch_dic["Sources"] = None
+    return ex_branch_dic
+    
+
+
 def obj_filter(obj_dic):
     output_dic = {}
     output_dic["Title"] = obj_dic["title"]
-    output_dic["Sources"] = obj_dic["url"]
+    tem_sources = []
+    url_list = [obj_dic["url"]]
+    source_url_list = [obj_dic["source_url"]]
+    if (url_list[0] != None) and (source_url_list[0] != None):
+        tem_sources = url_list + source_url_list
+        
+    elif (url_list[0] == None) and (obj_dic["source_url"] == None):
+        tem_sources = []
+    else:
+        if obj_dic["url"] != None:
+            tem_sources = url_list
+        else: 
+            tem_sources = source_url_list
     output_dic["Content"] = obj_dic["content"].replace(" ", "")
     if "imag" in obj_dic.keys():
         output_dic["Image"] = obj_dic["image"]
     # output_dic["Date"] = obj_dic["date"]
-    output_dic.update(fil_except_branch(
-        output_dic["Content"], output_dic["Sources"]))
+    output_dic.update(fil_ex_branch(output_dic["Content"],tem_sources))
     output_dic.update(fil_branch(output_dic["Content"]))
     return output_dic
 
@@ -206,12 +227,6 @@ with open('./re_filter_tool/filter_after.json', 'w', encoding='utf-8') as jsonfi
 jsonfile.close()
 
 update_only_one()
-
-# testre = re.split("[^0-9]", "哈摟哀呢區111年10月5日")
-# ttest = [i for i in testre if i != ""]
-# print(datetime.date(int(ttest[0]),int(ttest[1]),int(ttest[2])))
-
-
 
 
 # filter date from original floder["date"]
